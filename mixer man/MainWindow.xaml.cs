63766ad242061b1e66e1a8748fd7265026da5da6
gods1son <tolulopeibiyode@gmail.com>
2016-12-23 22:25:44 -0500

    mix color app

---
+++ mixer man/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mixer_man
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sldRed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        { 
+            var r = (byte)sldRed.Value;
+            var g = (byte)sldGreen.Value;
+            var b = (byte)sldBlue.Value;
+            var color = Color.FromRgb(r, g, b);
+            if (cnvColor != null)
+            {
+                cnvColor.Background = new SolidColorBrush(color);
+            }
+        }
+    }
+}
